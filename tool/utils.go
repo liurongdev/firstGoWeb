de12badf26cdc694197abab37633066b527f8cc9
liurong <1069919773@qq.com>
2025-03-20 14:30:17 +0800

    接入http接口机制

--- tool/utils.go
+++ tool/utils.go
@@ -1,11 +1,25 @@
 package tool
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/liurongdev/firstGoWeb/handle"
+	"github.com/liurongdev/firstGoWeb/middleware/logger"
+	"net/http"
 )
 
 func HelloModule(user *handle.User) string {
 	fmt.Println(user.Name)
 	return user.Id + user.Name + user.Email + "local version test"
+}
+
+func callRemote(url string, req interface{}) interface{} {
+	b, _ := json.Marshal(req)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(b))
+	defer resp.Body.Close()
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	return resp
 }
